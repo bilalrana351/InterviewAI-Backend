b11a63fa25e6ec0565cb77c8280b3f8dcf278e20
Bilal Rana <bilalrana351@gmail.com>
2025-05-11 01:47:30 +0500

    Specified cors option

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const app = express();
 const PORT = process.env.PORT || DEFAULT_PORT;
 app.use(
   cors({
-    origin: "*",
+    origin: "https://interview-ai-client-nine.vercel.app",
     credentials: true,
   })
 );
